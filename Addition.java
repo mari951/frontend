71f83aca6e1c41b1650e3f4cc9a59b5f6b92cf62
mari951 <marieaz843@gmail.com>
2024-08-14 11:14:53 -0300

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,28 @@
+import javax.print.attribute.standard.JobHoldUntil;
+import javax.swing.JOptionPane;
+
+public class Addition
+
+{
+
+public static void main(String[]args)
+
+{
+    String firstNumber = //numero 
+
+JOptionPane.showInputDialog(message:"Enter first integer");
+String secondNumber =//numero
+JobHoldUntil.showInputDialog(message:"Enter second integer");
+
+int number1 = Integer.perseInt(firstNumber);
+int number2 = Integer.parseInt(secondNumber);
+
+
+int sun = number1 + number2;
+
+ JOptionPane.showMessageDialog(parenteCompont:null,"The sum is"+ sum,
+ title:"Sum of Two Integers",JOptionPane.PLAIN_MESSAGE);
+
+}//
+
+
